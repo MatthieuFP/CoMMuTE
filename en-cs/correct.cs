862de1a934d5407803b637c8db34e418b1db51c5
mfuteral <mfuteral@MP1160-PRO.local>
2023-05-26 13:16:07 +0200

    CoMMuTEv2

--- en-cs/correct.cs
+++ en-cs/correct.cs
@@ -98,3 +98,211 @@ Vypadá, že si to rande užívá.
 Vypadá, že mu ta datle chutná.
 Nejez kosti!
 Nejez rybí kosti!
+Skvělé, je sezóna kriketu!
+Skvělé, je sezóna cvrčků!
+Obávám se, že se nám to zase zaseklo.
+Obávám se, že máme další zácpu.
+Ano, je to jeho bar.
+Ano, je to jeho kovová tyč na vzpírání.
+Ministryně pronáší řeč a drží knihu.
+Duchovní pronáší řeč a drží knihu.
+Pustil se do spravování svého spodního prádla.
+Pustil se do opravy svých šuplíků.
+Víš, kde jsou mé kalhoty?
+Víš, kde jsou mé trenýrky?
+Vytvořil jsi tabulku?
+Vyrobil jsi stůl?
+Dejte mi účet.
+Dejte mi bankovku.
+Před snězením vždy odstraním kůži.
+Před snězením vždy odstraním slupku.
+Je čas na bazén!
+Je čas na kulečník!
+Vypadá to, že kolo je nějak zablokované.
+Vypadá to, že volant je nějak zablokovaný.
+Tvůj punč je úžasný!
+Tvůj úder je úžasný!
+Můj manžel si nedávno koupil choppera.
+Můj manžel si nedávno koupil vrtulník.
+Koupila sis nový šátek, že?
+Koupila sis novou šálu, že?
+Jsme pod klenbou stromů. 
+Jsme pod pergolou.
+Dudlíky se musí vyčistit.
+Figuríny se musí vyčistit.
+Chce mi ukázat své místo.
+Chce mi ukázat svůj pupínek.
+Viděl jsi mou brož?
+Viděl jsi můj špendlík?
+Váha je rozbitá.
+Šupina je poškozená.
+Někdo by měl umýt výlohu.
+Někdo by měl umýt okno.
+Nenech děti hrát si na lešení!
+Nenech děti hrát si na dvorku!
+Špunt přestal fungovat, mohl bys ho spravit?
+Zástrčka přestala fungovat, mohl bys ji spravit?
+To máš malý kámen.
+To máš malou pecku.
+Spadla mi svorka na podlahu, ale nejsem si jistá kam.
+Spadla mi sponka na podlahu, ale nejsem si jistá kam.
+Podej mi těsto!
+Podej mi prachy!
+Musíš se zbavit těch jasanů.
+Musíš se zbavit popela.
+Má housky v troubě.
+Má buchtu v troubě.
+Má míčky!
+Má koule!
+Nezahrávej si s revizorem!
+Nehraj si s ovladačem!
+Jelen se jen díval přímo na mě.
+Laň se jen dívala přímo na mě.
+Srna na poli.
+Králice na poli.
+Nedívej se na mouchu!
+Nedívej se na poklopec!
+Koupil si vlastní letadlo.
+Koupil si vlastní hoblík.
+Podívej se na její obličej, má ruměnec ve tváři!
+Podívej se na její obličej, má fleš!
+Tvoje dáseň nevypadá dobře!
+Tvoje žvýkačka nevypadá dobře!
+To musí být duchové!
+To musí být kořalky!
+Platýsi zapáchali. 
+Podrážky zapáchaly.
+Osoba ve žluté bundě na něco čeká.
+Osoba ve žluté vestě na něco čeká.
+Vypadá to, že jsem zhubla pár liber.
+Vypadá to, že jsem přišla o pár liber.
+Občas trubky opravdu musíš pořádně vyčistit.
+Občas dudy opravdu musíš pořádně vyčistit.
+Klínek je celý shnilý.
+Americké brambory jsou celé shnilé.
+Jeho bota mu na hřišti prostě spadla.
+Jeho podkova na hřišti prostě spadla.
+Dej mi jeho záznam prosím.
+Dej mi jeho desku prosím.
+To je úžasný vlak, že?
+To je úžasná vlečka, že?
+Viděl jsi pravítko? 
+Viděl jsi vládce?
+Podívej se pod kapotu!
+Podívej se pod kuklu!
+Subway? Je to na tobě.
+Metro? Je to na tobě.
+Co je to za znak? Nikdy jsem ho neviděl.
+Co je to za značku? Nikdy jsem ji neviděl.
+Podívej, má poraněnou nohu.
+Podívej, má zranění na noze.
+Je to zkušený žokej.
+Je to zkušený jezdec.
+Minulý víkend jsme šli na závodní dráhu.
+Minulý víkend jsme šli na dostihovou dráhu.
+Wow, ten model je krásný!
+Wow, ta modelka je krásná!
+Dokovací stanice je ze dřeva a je celkem odolná.
+Molo je ze dřeva a je celkem bytelné.
+Proč mám pocit, že se mu líbí dril?
+Proč mám pocit, že se mu líbí vrtačky?
+Sedí na kmeni.
+Sedí na kufru.
+Ples trval opravdu dlouho, stálo to za ty peníze!
+Míč vydržel opravdu dlouho, stál za ty peníze!
+Vzal vodítko.
+Ujal se vedení.
+Máš nějakou kolu?
+Máš nějakej koks?
+Náhubek tohoto psa je zvláštní.
+Čenich tohoto psa je zvláštní.
+Neslyšíš ten řev?
+Neslyšíš to burácení?
+Podej mi piky.
+Podej mi rýč.
+Na palubě si dal dobré chlazené pivo.
+Na terase si dal dobré chlazené pivo.
+Nemůžu se dočkat – víš, že je to mé oblíbené představení!
+Nemůžu se dočkat – víš, že je to můj oblíbený pořad!
+Hrobka je teď uzavřená.
+Trezor je teď uzavřený.
+Dej to do košíku.
+Dej to do koše.
+Upustil jsem kapsli. 
+Upustil jsem uzávěr.
+Dostal střelnou ránu do ruky a ihned pocítil ostrou bolest.
+Dostal injekci do ruky a ihned pocítil ostrou bolest.
+Viděl jsem někoho s hůlkou jít po ulici.
+Viděl jsem někoho s tyčí jít po ulici.
+Západka je rozbitá, mohl bys ji vyměnit?
+Šroub je rozbitý, mohl bys ho vyměnit?
+Když jsem se nastěhoval do svého nového bytu, našel jsem krabičku, kterou tu nechal předchozí nájemník.
+Když jsem se nastěhoval do svého nového bytu, našel jsem krabici, kterou tu nechal předchozí nájemník.
+Jsou zvyklí veslovat.
+Jsou zvyklí se hádat.
+To je dobrý švih!
+To je dobrá houpačka!
+Přeskočil plot.
+Přeskočil překážku.
+Zanech prosím na stole vzkaz.
+Zanech prosím na stole bankovku.
+Máme nového nadhazovače.
+Máme nový džbán.
+Jeden z orgánů bohužel nefunguje správně.
+Jedny varhany bohužel nefungují správně.
+Je známý svými krásnými růžemi.
+Je známý svými krásnými rozetami.
+Můj manžel miluje squash!
+Můj manžel miluje dýně!
+Stříleli ve vsi dole u pošty.
+Natáčeli ve vsi dole u pošty.
+Potřebuje to opravdu dobře zamést.
+Potřebuje to opravdu dobře vymést.
+Vyměnili jsme trenéry.
+Přestoupili jsme do jiného vagónu.
+Dlaždice jsou staré a rozbité, potřebují vyměnit.
+Tašky jsou staré a rozbité, potřebují vyměnit.
+Podej mi tu konzervu prosím.
+Podej mi tu plechovku prosím.
+Dávej pozor, to železo je horké!
+Dávej pozor, ta žehlička je horká!
+Ještě nějaké máme, ale pro jistotu koupím pár dalších rolí.
+Ještě nějaké máme, ale pro jistotu koupím pár dalších housek.
+Chci se na ty brusle podívat zblízka.
+Chci se na toho rejnoka podívat zblízka.
+Jaké kuře!
+Jaký zbabělec!
+Vedle hlavního náměstí nebo toho malého?
+Vedle hlavního čtverce nebo toho malého?
+Kdybychom mohli prkýnko trochu poposunout, vytvořilo by to nějaký prostor.
+Kdybychom mohli herní desku trochu poposunout, vytvořilo by to nějaký prostor.
+Ví jak zacházet s pádlem.
+Ví jak zacházet s pálkou.
+Muž viděl, jak se ponořila. 
+Muž viděl její kachnu.
+Podívej se na ten záliv, je krásný!
+Podívej se na ten bobkový list, je krásný!
+Kolik stupňů? Dva stupně!
+Kolik titulů? Dva tituly!
+Neříkej mi, že máš zase problém s úlem?
+Neříkej mi, že máš zase problém s kopřivkou?
+Jak se ti přihodila taková skvrna?
+Jak jsi dostal takovou známku?
+Jenom jedna slza, ale stačilo to.
+Jenom jedno roztržení, ale stačilo to.
+Mimochodem klíč se trochu zasekává.
+Mimochodem klávesa se trochu zasekává.
+Nečekal jsem, že budeš tak chytrý!
+Nečekal jsem, že budeš tak elegantní!
+Ach, to je příliš pálivé!
+Ach, to je příliš horké!
+Ach, nestresuj se, je to jen průvan!
+Ach, nestresuj se, je to jen pracovní verze!
+Skořápka je prasklá.
+Krunýř je prasklý.
+Běhoun musel stát spoustu peněz!
+Koberec musel stát spoustu peněz!
+Udělej mi laskavost a ukliď ten kočičí záchod prosím.
+Udělej mi laskavost a ukliď ten tácek prosím. 
+Rám je vyroben ze dřeva.
+Krov je vyroben ze dřeva.
--- en-cs/incorrect.cs
+++ en-cs/incorrect.cs
@@ -98,3 +98,211 @@ Vypadá, že mu ta datle chutná.
 Vypadá, že si to rande užívá.
 Nejez rybí kosti!
 Nejez kosti!
+Skvělé, je sezóna cvrčků!
+Skvělé, je sezóna kriketu!
+Obávám se, že máme další zácpu.
+Obávám se, že se nám to zase zaseklo.
+Ano, je to jeho kovová tyč na vzpírání.
+Ano, je to jeho bar.
+Duchovní pronáší řeč a drží knihu.
+Ministryně pronáší řeč a drží knihu.
+Pustil se do opravy svých šuplíků.
+Pustil se do spravování svého spodního prádla.
+Víš, kde jsou mé trenýrky?
+Víš, kde jsou mé kalhoty?
+Vyrobil jsi stůl?
+Vytvořil jsi tabulku?
+Dejte mi bankovku.
+Dejte mi účet.
+Před snězením vždy odstraním slupku.
+Před snězením vždy odstraním kůži.
+Je čas na kulečník!
+Je čas na bazén!
+Vypadá to, že volant je nějak zablokovaný.
+Vypadá to, že kolo je nějak zablokované.
+Tvůj úder je úžasný!
+Tvůj punč je úžasný!
+Můj manžel si nedávno koupil vrtulník.
+Můj manžel si nedávno koupil choppera.
+Koupila sis novou šálu, že?
+Koupila sis nový šátek, že?
+Jsme pod pergolou.
+Jsme pod klenbou stromů. 
+Figuríny se musí vyčistit.
+Dudlíky se musí vyčistit.
+Chce mi ukázat svůj pupínek.
+Chce mi ukázat své místo.
+Viděl jsi můj špendlík?
+Viděl jsi mou brož?
+Šupina je poškozená.
+Váha je rozbitá.
+Někdo by měl umýt okno.
+Někdo by měl umýt výlohu.
+Nenech děti hrát si na dvorku!
+Nenech děti hrát si na lešení!
+Zástrčka přestala fungovat, mohl bys ji spravit?
+Špunt přestal fungovat, mohl bys ho spravit?
+To máš malou pecku.
+To máš malý kámen.
+Spadla mi sponka na podlahu, ale nejsem si jistá kam.
+Spadla mi svorka na podlahu, ale nejsem si jistá kam.
+Podej mi prachy!
+Podej mi těsto!
+Musíš se zbavit popela.
+Musíš se zbavit těch jasanů.
+Má buchtu v troubě.
+Má housky v troubě.
+Má koule!
+Má míčky!
+Nehraj si s ovladačem!
+Nezahrávej si s revizorem!
+Laň se jen dívala přímo na mě.
+Jelen se jen díval přímo na mě.
+Králice na poli.
+Srna na poli.
+Nedívej se na poklopec!
+Nedívej se na mouchu!
+Koupil si vlastní hoblík.
+Koupil si vlastní letadlo.
+Podívej se na její obličej, má fleš!
+Podívej se na její obličej, má ruměnec ve tváři!
+Tvoje žvýkačka nevypadá dobře!
+Tvoje dáseň nevypadá dobře!
+To musí být kořalky!
+To musí být duchové!
+Podrážky zapáchaly.
+Platýsi zapáchali. 
+Osoba ve žluté vestě na něco čeká.
+Osoba ve žluté bundě na něco čeká.
+Vypadá to, že jsem přišla o pár liber.
+Vypadá to, že jsem zhubla pár liber.
+Občas dudy opravdu musíš pořádně vyčistit.
+Občas trubky opravdu musíš pořádně vyčistit.
+Americké brambory jsou celé shnilé.
+Klínek je celý shnilý.
+Jeho podkova na hřišti prostě spadla.
+Jeho bota mu na hřišti prostě spadla.
+Dej mi jeho desku prosím.
+Dej mi jeho záznam prosím.
+To je úžasná vlečka, že?
+To je úžasný vlak, že?
+Viděl jsi vládce?
+Viděl jsi pravítko? 
+Podívej se pod kuklu!
+Podívej se pod kapotu!
+Metro? Je to na tobě.
+Subway? Je to na tobě.
+Co je to za značku? Nikdy jsem ji neviděl.
+Co je to za znak? Nikdy jsem ho neviděl.
+Podívej, má zranění na noze.
+Podívej, má poraněnou nohu.
+Je to zkušený jezdec.
+Je to zkušený žokej.
+Minulý víkend jsme šli na dostihovou dráhu.
+Minulý víkend jsme šli na závodní dráhu.
+Wow, ta modelka je krásná!
+Wow, ten model je krásný!
+Molo je ze dřeva a je celkem bytelné.
+Dokovací stanice je ze dřeva a je celkem odolná.
+Proč mám pocit, že se mu líbí vrtačky?
+Proč mám pocit, že se mu líbí dril?
+Sedí na kufru.
+Sedí na kmeni.
+Míč vydržel opravdu dlouho, stál za ty peníze!
+Ples trval opravdu dlouho, stálo to za ty peníze!
+Ujal se vedení.
+Vzal vodítko.
+Máš nějakej koks?
+Máš nějakou kolu?
+Čenich tohoto psa je zvláštní.
+Náhubek tohoto psa je zvláštní.
+Neslyšíš to burácení?
+Neslyšíš ten řev?
+Podej mi rýč.
+Podej mi piky.
+Na terase si dal dobré chlazené pivo.
+Na palubě si dal dobré chlazené pivo.
+Nemůžu se dočkat – víš, že je to můj oblíbený pořad!
+Nemůžu se dočkat – víš, že je to mé oblíbené představení!
+Trezor je teď uzavřený.
+Hrobka je teď uzavřená.
+Dej to do koše.
+Dej to do košíku.
+Upustil jsem uzávěr.
+Upustil jsem kapsli. 
+Dostal injekci do ruky a ihned pocítil ostrou bolest.
+Dostal střelnou ránu do ruky a ihned pocítil ostrou bolest.
+Viděl jsem někoho s tyčí jít po ulici.
+Viděl jsem někoho s hůlkou jít po ulici.
+Šroub je rozbitý, mohl bys ho vyměnit?
+Západka je rozbitá, mohl bys ji vyměnit?
+Když jsem se nastěhoval do svého nového bytu, našel jsem krabici, kterou tu nechal předchozí nájemník.
+Když jsem se nastěhoval do svého nového bytu, našel jsem krabičku, kterou tu nechal předchozí nájemník.
+Jsou zvyklí se hádat.
+Jsou zvyklí veslovat.
+To je dobrá houpačka!
+To je dobrý švih!
+Přeskočil překážku.
+Přeskočil plot.
+Zanech prosím na stole bankovku.
+Zanech prosím na stole vzkaz.
+Máme nový džbán.
+Máme nového nadhazovače.
+Jedny varhany bohužel nefungují správně.
+Jeden z orgánů bohužel nefunguje správně.
+Je známý svými krásnými rozetami.
+Je známý svými krásnými růžemi.
+Můj manžel miluje dýně!
+Můj manžel miluje squash!
+Natáčeli ve vsi dole u pošty.
+Stříleli ve vsi dole u pošty.
+Potřebuje to opravdu dobře vymést.
+Potřebuje to opravdu dobře zamést.
+Přestoupili jsme do jiného vagónu.
+Vyměnili jsme trenéry.
+Tašky jsou staré a rozbité, potřebují vyměnit.
+Dlaždice jsou staré a rozbité, potřebují vyměnit.
+Podej mi tu plechovku prosím.
+Podej mi tu konzervu prosím.
+Dávej pozor, ta žehlička je horká!
+Dávej pozor, to železo je horké!
+Ještě nějaké máme, ale pro jistotu koupím pár dalších housek.
+Ještě nějaké máme, ale pro jistotu koupím pár dalších rolí.
+Chci se na toho rejnoka podívat zblízka.
+Chci se na ty brusle podívat zblízka.
+Jaký zbabělec!
+Jaké kuře!
+Vedle hlavního čtverce nebo toho malého?
+Vedle hlavního náměstí nebo toho malého?
+Kdybychom mohli herní desku trochu poposunout, vytvořilo by to nějaký prostor.
+Kdybychom mohli prkýnko trochu poposunout, vytvořilo by to nějaký prostor.
+Ví jak zacházet s pálkou.
+Ví jak zacházet s pádlem.
+Muž viděl její kachnu.
+Muž viděl, jak se ponořila. 
+Podívej se na ten bobkový list, je krásný!
+Podívej se na ten záliv, je krásný!
+Kolik titulů? Dva tituly!
+Kolik stupňů? Dva stupně!
+Neříkej mi, že máš zase problém s kopřivkou?
+Neříkej mi, že máš zase problém s úlem?
+Jak jsi dostal takovou známku?
+Jak se ti přihodila taková skvrna?
+Jenom jedno roztržení, ale stačilo to.
+Jenom jedna slza, ale stačilo to.
+Mimochodem klávesa se trochu zasekává.
+Mimochodem klíč se trochu zasekává.
+Nečekal jsem, že budeš tak elegantní!
+Nečekal jsem, že budeš tak chytrý!
+Ach, to je příliš horké!
+Ach, to je příliš pálivé!
+Ach, nestresuj se, je to jen pracovní verze!
+Ach, nestresuj se, je to jen průvan!
+Krunýř je prasklý.
+Skořápka je prasklá.
+Koberec musel stát spoustu peněz!
+Běhoun musel stát spoustu peněz!
+Udělej mi laskavost a ukliď ten tácek prosím. 
+Udělej mi laskavost a ukliď ten kočičí záchod prosím.
+Krov je vyroben ze dřeva.
+Rám je vyroben ze dřeva.
